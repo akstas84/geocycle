5abc5d61b7c6222ce817ff09090b997818714b7d
Stanislav <akstas84@users.noreply.github.com>
2024-04-01 20:26:14 +0300

    first commit

---
+++ src/main/java/com/telebot/geocycle/GeocycleApplication.java
@@ -0,0 +1,13 @@
+package com.telebot.geocycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeocycleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telebot/geocycle/GeocycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telebot.geocycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
